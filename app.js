c1fd4a122ed1b82b85ba4b6100be7978764c52ef
sagar2526 <sagarhinge01@gmail.com>
2018-09-05 20:03:26 +0530

    user controller and api calls with mongoose connection added

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const userController = require('./controllers/user')
+
 
 var app = express();
 
@@ -16,5 +21,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+mongoose.connect('mongodb://localhost:27017/quora')
+mongoose.connection.on('error', (error) => console.error(error))
+mongoose.connection.on('open', () => console.log("success in connecting to mongodb"))
+
+app.get('/api/v1/users', userController.getAllUsers)
+app.post('/api/v1/users', userController.postNewUsers)
+app.put('/api/v1/users/:id', userController.updateUsersById)
+app.delete('/api/v1/users/:id', userController.delUsersById)
+
+app.listen(4040, () => console.log('Express server at 4040'))
 
 module.exports = app;
